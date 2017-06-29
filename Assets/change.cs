aeb3a52194afb2dc39d59a9a1e6f311d1c3772b3
shailendra60 <shankaj60@gmail.com>
2017-06-29 12:15:18 +0530

    First Commit

---
+++ Assets/change.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class change : MonoBehaviour {
+
+	// Use this for initialization
+	public void changescene(string changetoscene)
+	{
+		SceneManager.LoadScene(changetoscene);
+	}
+}
---
+++ Assets/dontdestroy.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dontdestroy : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+       // GameObject[] objs = GameObject.FindGameObjectsWithTag("music");
+      //  if (objs.Length > 1)
+//Destroy(this.gameObject);
+        DontDestroyOnLoad(this.gameObject);
+		
+	}
+	
+	}
